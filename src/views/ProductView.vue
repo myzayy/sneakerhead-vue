d4f9d847e11a11df8fc9a7e04264399288052127
myzayy <116192354+myzayy@users.noreply.github.com>
2026-01-17 22:46:48 +0200

    added products list to ProductView
    Added dynamic description for different products. Imported Best Sellers list from ProductCard. Moved code block from mounted() to his own method loadProduct(). And also added watch() that start loadProduct() method.

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -19,7 +19,7 @@
                     <div class="product-desc">
                         <h3>{{ product.title }}</h3>
                         <p class="price"><span>${{ product.price }}</span></p>
-                        <p>Item description</p>
+                        <p>{{ product.description }}</p>
                         <p>
                         <button @click="AddToCart" class="btn btn-primary btn-addtocart">
                             <i class="icon-shopping-cart"></i> Add to Cart
@@ -34,19 +34,50 @@
             </div>
         </div>
     </div>
+    <div class="colorlib-product">
+    	<div class="container">
+      		<div class="row">
+        		<div class="col-sm-8 offset-sm-2 text-center colorlib-heading">
+          			<h2>Best Sellers</h2>
+       			</div>
+      		</div>
+      
+      		<div class="row row-pb-md">
+        		<ProductCard v-for="item in products"
+				:key="item.id"
+				:id="item.id"
+				:title="item.title"
+				:price="item.price"
+				:image="item.image"
+
+				:productObj="item"  
+        		@add-to-cart="addToCartParent">
+				
+				</ProductCard>
+				<!-- :productObj="item"  put whole item in pocket -->
+				<!-- @add-to-cart="addToCartParent" listener, call addToCartParent() -->
+      		</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import products from '../products.json';
+    import productsData from '../products.json';
     import { useCartStore } from '@/stores/cartStore';
     import { mapStores } from 'pinia';
+    import ProductCard from '@/components/ProductCard.vue';
 
     export default {
         name: 'ProductView',
+        
+        components: {
+            ProductCard
+        },
         data() {
             return {
                 product: null,
-                loading: true
+                loading: true,
+                products: productsData
             }
         },
         
@@ -57,19 +88,7 @@
         },
 
         mounted() {
-            
-            setTimeout(() => {
-                // take id from link-addres
-                const id = this.$route.params.id;
-
-                // seek product from json by id
-                this.product = products.find(p => p.id == id);
-
-                this.loading = false;
-            },  500);
-            
-
-            
+            this.loadProduct();
         },
         
         methods: {
@@ -77,6 +96,33 @@
                 this.cartStore.addToCart(this.product);
 
                 alert("Item added to cart!")
+            },
+            loadProduct() {
+                this.loading = true;
+                // scrolls user to page top when reddirect to other item
+                window.scrollTo(0,0);
+
+                setTimeout(() => {
+                    // take id from link-addres
+                    const id = this.$route.params.id;
+
+                    // seek product from json by id
+                    this.product = productsData.find(p => p.id == id);
+
+                    this.loading = false;
+                },  500);
+            },
+            addToCartParent(product) {
+				console.log("Item moved succesfully");
+				
+				this.cartStore.addToCart(product);
+
+				alert(`${product.title} added to cart!`);
+			}
+        },
+        watch: { // watch needed to restart monted for new id
+            '$route.params.id': function() {
+                this.loadProduct();
             }
         }
     }
